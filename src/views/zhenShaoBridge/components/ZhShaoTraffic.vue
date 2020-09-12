5dfb60f8ee1c86138ae828484ccd96d12e13dcd4
吕冲 <1915156922@qq.com>
2020-09-12 21:18:01 +0800

    traffic

--- src/views/zhenShaoBridge/components/ZhShaoTraffic.vue
+++ src/views/zhenShaoBridge/components/ZhShaoTraffic.vue
@@ -3,7 +3,7 @@
     <!-- 面包屑导航 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>郑少高速桥</el-breadcrumb-item>
-      <el-breadcrumb-item>车流分析</el-breadcrumb-item>
+      <el-breadcrumb-item>车流分析1</el-breadcrumb-item>
     </el-breadcrumb>
 
     <!-- 内容区域 -->
